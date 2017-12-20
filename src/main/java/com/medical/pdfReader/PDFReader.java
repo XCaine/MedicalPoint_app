c7180c4067f44457051c4f8bf2661857572d277e
Jakub Wesołowski <wesolowskijakub7@gmail.com>
2017-12-20 22:30:57 +0100

    Updated PDF parser - support for more voivodeships

--- src/main/java/com/medical/pdfReader/PDFReader.java
+++ src/main/java/com/medical/pdfReader/PDFReader.java
@@ -29,33 +29,45 @@ public class PDFReader {
     }
 
     static void parseLine() {
+        line = line.toUpperCase();
         if(skipFirstLines > 0)
             --skipFirstLines;
-        else if(line.contains("1.3. ")) {
+        else if(line.contains("1.3. ") || line.contains("1.3 ")) {
             saveMedicalPoint();
 
-            String nazwaZakladu = line.replace("1.3. Nazwa zakładu leczniczego: ", "");
+            String nazwaZakladu = line.replace("1.3. NAZWA ZAKŁADU LECZNICZEGO: ", "");
             medicalPoint.nazwaZakladu = nazwaZakladu;
         }
-        else if(line.contains("1.4. ")) {
-            String adresZakladu = line.replace("1.4. Adres zakładu leczniczego: ", "");
+        else if(line.contains("1.4. ") || line.contains("1.4 ")) {
+            String adresZakladu = line.replace("1.4. ADRES ZAKŁADU LECZNICZEGO: ", "");
             medicalPoint.adresZakladu = adresZakladu;
         }
-        else if(line.toUpperCase().contains("PORADA SPECJALISTYCZNA")) {
+        else if(line.contains("PORADA SPECJALISTYCZNA")) {
             int index = line.indexOf("3");
+            if(index == -1)
+                index = 0;
 
             String specjalnosc = line.substring(0, Math.min(line.length(), index));
-            specjalnosc = specjalnosc.toUpperCase();
             specjalnosc = specjalnosc.replace("PORADA SPECJALISTYCZNA – ", "");
+            specjalnosc = specjalnosc.replace("PORADA SPECJALISTYCZNA - ", ""); // Dolnośląskie ma chyba inny myślnik albo biały znak
 
             if(!medicalPoint.profile.contains(specjalnosc))
                 medicalPoint.profile.add(specjalnosc);
         }
         else if(line.matches(profileRegex)) {
-            int index = line.indexOf("2");
-            String Profil = line.substring(0, Math.min(line.length(), index));
+            if(line.contains("IZBA"))
+                medicalPoint.izbaPrzyjec = true;
+            else if(line.contains("SOR") || line.contains("SZPITALNY ODDZIAŁ RATUNKOWY"))
+                medicalPoint.sor = true;
+            else {
+                int index = line.indexOf("2");
+                String profil = line.substring(0, Math.min(line.length(), index));
 
-            medicalPoint.profile.add(Profil);
+                medicalPoint.profile.add(profil);
+            }
+        }
+        else if(line.contains("ŚWIADCZENIA NOCNEJ I ŚWIĄTECZNEJ")) {
+            medicalPoint.nocnaSwiatecznaPomoc = true;
         }
     }
 
@@ -63,6 +75,7 @@ public class PDFReader {
         try (BufferedReader reader = new BufferedReader(new StringReader(text))) {
             while ((line = reader.readLine()) != null) {
                 parseLine();
+                //System.out.println(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,7 +83,34 @@ public class PDFReader {
     }
 
     public static void main(String[] args) throws Exception {
-        PDDocument document = PDDocument.load(new File("pdf/dolnoslaskie.pdf"));
+        /*
+        Kompatybilność z PDFami:
+        7/16
+
+        Działają:
+            Mazowieckie
+            Dolnośląskie
+            Kujawsko-Pomorskie
+            Podkarpackie
+            Swiętokrzyskie
+            Warminsko-Mazurskie
+            Wielkopolskie
+
+        Wymagają poprawek:
+            Lodzkie
+            Lubelskie
+            Lubuskie
+            Malopolskie
+            Opolskie
+            Podlaskie
+            Pomorskie
+            Sląskie
+            Zachodniopomorskie
+
+            TO DO:
+                - x dla dzieci == x dziecięca, usunięcie powtórzeń jeśli gdzieś są
+        */
+        PDDocument document = PDDocument.load(new File("pdf/mazowieckie.pdf"));
         if (!document.isEncrypted()) {
             PDFTextStripper stripper = new PDFTextStripper();
             String text = stripper.getText(document);
@@ -79,6 +119,5 @@ public class PDFReader {
             printMedicalPoints();
         }
         document.close();
-
     }
 }
--- src/main/java/com/medical/pdfReader/ParsedMedicalPoint.java
+++ src/main/java/com/medical/pdfReader/ParsedMedicalPoint.java
@@ -5,6 +5,9 @@ import java.util.Vector;
 public class ParsedMedicalPoint {
     String nazwaZakladu, adresZakladu;
     Vector<String> profile = new Vector<String>();
+    boolean sor = false;
+    boolean nocnaSwiatecznaPomoc = false;
+    boolean izbaPrzyjec = false;
 
     void saveToDatabase()
     {
@@ -21,6 +24,9 @@ public class ParsedMedicalPoint {
         for(int j=0; j<profile.size(); j++) {
             System.out.println("    " + profile.get(j));
         }
+        System.out.println("SOR:                        " + sor);
+        System.out.println("IzbaPrzyjec:                " + izbaPrzyjec);
+        System.out.println("NocnaiSwiatecznaPomoc:      " + nocnaSwiatecznaPomoc);
         System.out.println("\n");
     }
 
