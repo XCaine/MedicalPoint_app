00a0046c3339a0a84737ddf969b70b8534976503
Paweł Gąsecki <pawelgasecki1996@gmail.com>
2018-01-03 23:56:03 +0100

    web for new controllers

--- src/main/web/js/controllers.js
+++ src/main/web/js/controllers.js
@@ -8,43 +8,96 @@ medicalApp.controller('homeController',['$scope','$http', function($scope,$http)
 medicalApp.controller('medicalpointsController',['$scope','$http', function($scope,$http){
         
     $scope.results='';
-    $scope.ID='20';
+    $scope.ID='73';
     
     $scope.request = function () {    
     $http.get("http://localhost:8080/medicalpoint/searchbyid/"+$scope.ID)
         .success(function(result) { 
             $scope.results=result; 
+            $scope.ID='';
         });       
     }
 }]);
 //panel for data insert
 medicalApp.controller('datainsertController',['$scope','$http', function($scope,$http){
+
+    $scope.jsonrequest='';
+    $scope.hospital = {
+        "name": "Szpital Psychiatryczny 'Elka' ",
+        "city": "Warszawa",
+        "province": "mazowieckie",
+        "country": "Polska",
+        
+        "address":{
+            "street_name": "Nowowiejska",
+            "street_number": "15/19",
+            "postal_code": "00-665"
+        }
+    };
     
     $scope.name='';
     $scope.results='';
     
     $scope.request = function () {
-    $http.get("http://localhost:8080/medicalpoint/addbyname/"+$scope.name)
+    $http.get("http://localhost:8080/admin/medicalpoint/addwithname/"+$scope.name)
         .success(function(result) { 
-            $scope.results=result; 
+            $scope.results=result;
+            $scope.name='';
         });                  
+    }
+    
+    $scope.submit = function () {
+
+        $scope.jsonrequest=JSON.stringify($scope.hospital);
+        
+        $scope.hospital.name='';
+        $scope.hospital.city='';
+        $scope.hospital.province='';
+        $scope.hospital.country='';
+        $scope.hospital.street_name='';
+        $scope.hospital.street_number='';
+        $scope.hospital.postal_code='';
+        
+        $http.post("http://localhost:8080/admin/medicalpoint/addwithJSON", $scope.jsonrequest);
     }
 
 }]);
 //page when a user can get help
 medicalApp.controller('gethelpController',['$scope','$http', function($scope,$http){
 
-    $scope.city='warszawa';
-    $scope.street='nowowiejska';
+    $scope.city='Warszawa';
+    $scope.street='Nowowiejska';
     $scope.number='15/19';
+    $scope.illness='Eye pain';
     $scope.results='';
+    $scope.finalresults='';
     
     $scope.request = function () {
     $http.get("https://maps.googleapis.com/maps/api/geocode/json?address="+$scope.city+"+"+$scope.street+"+"+$scope.number+"+"+"&key=AIzaSyCPoLpNU2TEKnVNQOrrJH8SlUqdv-93LN0")
         .success(function(result) { 
-            $scope.results=result; 
-        });                  
-    }
+            $scope.results=result;
+        
+            $http.get("http://localhost:8080/findmedicalpoint/findmedicalpoint/", {
+            params: {
+//            lat:'52.2192637',
+//            lon:'21.0129685',
+//            illness:'Eye pain',
+//            city:'Warszawa',
+//            province:'mazowieckie'
+                    lat: $scope.results.results[0].geometry.location.lat,
+                    lon: $scope.results.results[0].geometry.location.lng,
+                    illness: $scope.illness,
+                    city: $scope.results.results[0].address_components[3].long_name,
+                    province: $scope.results.results[0].address_components[5].long_name
+                    }
+                })
+        
+                .success(function(result) {
+                    $scope.finalresults=result;
+                });   
+            });    
+        }
+    
         
 }]);
 //information about the app
--- src/main/web/readme.txt
+++ src/main/web/readme.txt
@@ -4,11 +4,20 @@ Chrome won't allow http request to same domain
 as both server and web app are running locally                  
 you have to install allow-control-allow-origin chrome extension to chrome
 https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/related?hl=en-US 
+
+actually extension from that link stopped working for my request for some reason xd
+i installed other cors extension called "toggle cors"; you can look it up if you dont want to 
+manually disable web security in your chrome session
+
 2. 
 I highly recommend using brackets (text editor) live preview to use the UI, opera and chrome have
 problems with loading files from local domain ( same error as mentioned above )
 
 While you have brackets installed just open file->open folder->select web folder->select live preview(that little icon on top right)
 That should open the new chrome window with app running on one of the local ports
+
+ALSO
+chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
+pasting this command into windows run section should solve the problem
 
 3. both angularjs and bootstrap and added locally cause i sometimes my browser refused to load hem from online sources
