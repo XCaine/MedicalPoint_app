40dbba33736dcda027054f970283fed5c7905364
jferec <janferec@gmail.com>
2018-01-20 23:36:55 +0100

    AWS server up :)

--- src/main/java/com/medical/controller/HelloController.java
+++ src/main/java/com/medical/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.medical.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
     public String index()
     {
-        return "Greetings from Spring Boot";
-    }
-}
+        return "index.html";
+        }
+        }
+
